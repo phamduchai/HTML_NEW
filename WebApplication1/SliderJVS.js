3d18c59c4882b3207ad091df951fde938491aa12
MINHPHUONG1409 <nguyenminhphuong1409@gmail.com>
2023-10-10 17:25:39 +0700

    New change

---
+++ WebApplication1/SliderJVS.js
@@ -0,0 +1,28 @@
+﻿const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+let currentIndex = 0;
+
+function showSlide(index) {
+    if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else if (index >= slides.length) {
+        currentIndex = 0;
+    }
+    const translateX = -currentIndex * 100;
+    slider.style.transform = `translateX(${translateX}%)`;
+}
+
+prevButton.addEventListener('click', () => {
+    currentIndex--;
+    showSlide(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex++;
+    showSlide(currentIndex);
+});
+
+showSlide(currentIndex);
